b04dc9a14beb16441a711ae62f451a5ae166b566
Salim Uzun <salimaniatv@gmail.com>
2021-09-17 00:57:52 +0300

    Initial Commit

---
+++ Appcent-NewsAppTests/Appcent_NewsAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  Appcent_NewsAppTests.swift
+//  Appcent-NewsAppTests
+//
+//  Created by Salim Uzun on 17.09.2021.
+//
+
+import XCTest
+@testable import Appcent_NewsApp
+
+class Appcent_NewsAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
